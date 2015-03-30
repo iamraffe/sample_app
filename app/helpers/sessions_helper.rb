747f3c3f158f27e6114e1f9667860b04aa955d0b
rafaramirez <raffe90@gmail.com>
2015-03-30 15:20:39 +0200

    Add account activations

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,16 +18,16 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
     end
   end
 
- / def current_user?(user)
+  def current_user?(user)
     user == current_user
-  end /
+  end 
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
